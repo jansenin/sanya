a39b17e3f069b7754be63e065215ea0ac1c586e7
jansenin <jansenin@tut.by>
2021-08-14 14:35:40 +0300

    initial

---
+++ script.js
@@ -0,0 +1,11 @@
+/*$("document").ready(function() {
+	$(".menu-option").click(function() {
+
+		if ($(this).hasClass("active")) {
+			$(".menu-option").removeClass("active");
+		} else {
+			$(".menu-option").removeClass("active");
+			$(this).toggleClass("active");
+		}
+	});
+});*/
